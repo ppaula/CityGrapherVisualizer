3233408adc9b6057cda911a12364063e1c1eca94
Artur <kawala.artek@gmail.com>
2018-11-03 14:19:59 +0100

    Reindent

--- scripts/map.js
+++ scripts/map.js
@@ -3,15 +3,15 @@ import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import {Map, View} from 'ol';
-import {fromLonLat} from 'ol/proj';
-import {getStyles} from './styles.js';
-import {addMarker} from './marker.js';
-import {addLine} from './line.js';
+import { Map, View } from 'ol';
+import { fromLonLat } from 'ol/proj';
+import { getStyles } from './styles.js';
+import { addMarker } from './marker.js';
+import { addLine } from './line.js';
 
 const cracowCenterCoords = [19.937398, 50.061696];
 const cracowRandomCoords = [19.887398, 50.081696];
- 
+
 const styles = getStyles();
 
 var features = [];
@@ -19,14 +19,14 @@ var features = [];
 addMarker(features, cracowCenterCoords);
 addLine(features, cracowCenterCoords, cracowRandomCoords);
 
-const vectorSource = new VectorSource({features: features});
+const vectorSource = new VectorSource({ features: features });
 
 const vectorLayer = new VectorLayer({
-      source: vectorSource,
-      style: function(feature) {
-          return styles[feature.get('type')];
-        }
-    });
+  source: vectorSource,
+  style: function (feature) {
+    return styles[feature.get('type')];
+  }
+});
 
 const map = new Map({
   target: 'OsmMap',
