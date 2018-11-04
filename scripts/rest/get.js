f39a82afaff66cfa97c9451f579e3c6a4ebb83dc
Artur <kawala.artek@gmail.com>
2018-11-04 21:52:13 +0100

    Refactor

--- scripts/rest/get.js
+++ scripts/rest/get.js
@@ -1,8 +1,5 @@
-async function getJsonData(url) {
+export async function getJsonData(url) {
     const response = await fetch(url);
     const jsonData = await response.json();
     return jsonData;
 }
-
-const _getJsonData = getJsonData;
-export { _getJsonData as getJsonData };
