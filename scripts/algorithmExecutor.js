c7b9a22f71565bd6a102fd5b16bdcaf5ef2a916d
Artur <kawala.artek@gmail.com>
2018-12-05 22:41:05 +0100

    + Create action on btn click

---
+++ scripts/algorithmExecutor.js
@@ -0,0 +1,7 @@
+import { draw } from './graph.js';
+
+const algorithmStarterButton = document.getElementById("algorithmStarterButton");
+
+algorithmStarterButton.onclick = function() {
+    console.log('starting algorithm!!!');
+}
