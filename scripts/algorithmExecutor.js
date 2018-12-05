98a262300a7cb4d7fdac8f1d67d876fbcc28489a
Artur <kawala.artek@gmail.com>
2018-12-05 22:55:48 +0100

    + Rename formVerifier to formValidator + Add method validate() checking form + Add basic logic of handling click btn

--- scripts/algorithmExecutor.js
+++ scripts/algorithmExecutor.js
@@ -1,7 +1,14 @@
 import { draw } from './graph.js';
+import { validate } from './formValidator.js';
 
 const algorithmStarterButton = document.getElementById("algorithmStarterButton");
 
 algorithmStarterButton.onclick = function() {
-    console.log('starting algorithm!!!');
+    if (validate()) {
+        const cityInput = document.getElementById("cityInput");
+        const cityName = cityInput.value;
+        console.log("run algorithm for city: " + cityName);
+    } else {
+        // TODO call here errorPrinter or sth else, most probably in another script file
+    }
 }
