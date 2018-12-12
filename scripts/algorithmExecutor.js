80951906c3f6c0b262c2760087ea93c7b8892581
Artur <kawala.artek@gmail.com>
2018-12-12 22:43:18 +0100

    + Implement waiting for response from A module and then getting it from DC module + Draw graph after  result is given + Now draw graphData, not cityData - rename variables according to given json data + Change port of algortihm module + Add info about new endpoint in DC module + Add new helper methods returning proper paths for new endpoint

--- scripts/algorithmExecutor.js
+++ scripts/algorithmExecutor.js
@@ -1,9 +1,13 @@
 import { validate } from './formValidator.js';
 import { getCityGraphUri } from './config/config.js';
+import { getUriForAlgorithmTaskResult } from './config/config.js';
 import { getJsonData } from './rest/get.js';
-import { drawDataAsGraph } from './graph.js';
+import { grawGraph } from './graph.js';
 
 const algorithmStarterButton = document.getElementById("algorithmStarterButton");
+
+const maxNumberOfRequestForCalculationStatus = 500;
+const millisecondsToWaitBetweenRequests = 1000;
 
 algorithmStarterButton.onclick = function() {
     if (validate()) {
@@ -11,9 +15,35 @@ algorithmStarterButton.onclick = function() {
         const cityName = cityInput.value;
         const cityGraphDataUri = getCityGraphUri(cityName);
         getJsonData(cityGraphDataUri)
-            .then(result => drawDataAsGraph(result))
-            .catch(error => console.log(error));
+        .then(result => {
+            getResultsFromAlgorithm(0, result['uri']);
+        })
+        .catch(error => console.log(error));
     } else {
         // TODO call here errorPrinter or sth else, most probably in another script file
     }
+}
+
+function getResultsFromAlgorithm(requestCounter, uri) {
+    getJsonData(uri).then(result => {
+        const calculationStatus = result['status'];
+        if (calculationStatus == "SUCCESS") {
+            getPositiveResultFromAlgorithm(result['taskId']);
+        } else if (requestCounter < maxNumberOfRequestForCalculationStatus) {
+            requestCounter++;
+            setTimeout(getResultsFromAlgorithm, millisecondsToWaitBetweenRequests, requestCounter, uri);
+        } else {
+            console.log('time exceeded');
+        }
+    })
+    .catch(error => console.log(error));
+}
+
+function getPositiveResultFromAlgorithm(taskId) {
+    const uri = getUriForAlgorithmTaskResult(taskId);
+    
+    getJsonData(uri).then(algorithmResult => {
+        grawGraph(algorithmResult);
+    })
+    .catch(error => console.log(error));
 }
--- scripts/graph.js
+++ scripts/graph.js
@@ -2,26 +2,26 @@ import { addMarker } from './marker.js';
 import { addLine } from './line.js';
 import { drawMap } from './map.js';
 
-export function drawDataAsGraph(graphData) {
-    const ways = graphData['ways'];
-    const nodes = graphData['nodes'];
+export function grawGraph(graphData) {
+    const edges = graphData['edges'];
+    const crossings = graphData['crossings'];
     let features = [];
     let mapCenterCoords;
 
-    ways.forEach(way => {
-        const wayNodes = way['nodes'];
+    edges.forEach(edge => {
+        const edgeCrossings = edge['crossings'];
 
-        const firstNode = wayNodes[0];
-        const firstNodeCoords = mapCenterCoords = [firstNode['lon'], firstNode['lat']];
-        addMarker(features, firstNodeCoords);
+        const firstCrossing = edgeCrossings[0];
+        const firstCrossingCoords = mapCenterCoords = [firstCrossing['lon'], firstCrossing['lat']];
+        addMarker(features, firstCrossingCoords);
 
-        for (let i = 1; i < wayNodes.length; ++i) {
-            const nodeFrom = wayNodes[i - 1];
-            const nodeFromCoords = [nodeFrom['lon'], nodeFrom['lat']];
-            const nodeTo = wayNodes[i];
-            const nodeToCoords = [nodeTo['lon'], nodeTo['lat']];
-            addLine(features, nodeFromCoords, nodeToCoords);
-            addMarker(features, nodeToCoords);
+        for (let i = 1; i < edgeCrossings.length; ++i) {
+            const crossingFrom = edgeCrossings[i - 1];
+            const crossingFromCoords = [crossingFrom['lon'], crossingFrom['lat']];
+            const crossingTo = edgeCrossings[i];
+            const crossingToCoords = [crossingTo['lon'], crossingTo['lat']];
+            addLine(features, crossingFromCoords, crossingToCoords);
+            addMarker(features, crossingToCoords);
         }
     });
 
