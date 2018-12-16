9d017ce6a87eb082c4ba2483b9b67261fbc8b664
Artur <kawala.artek@gmail.com>
2018-12-16 17:00:05 +0100

    Add sending DELETE method to cancel algorithm task

--- scripts/algorithmExecutor.js
+++ scripts/algorithmExecutor.js
@@ -2,6 +2,7 @@ import { validate } from './formValidator.js';
 import { getCityGraphUri } from './config/config.js';
 import { getUriForAlgorithmTaskResult } from './config/config.js';
 import { getJsonData } from './rest/get.js';
+import { deleteForUri } from './rest/delete.js';
 import { drawGraph } from './graph.js';
 
 const algorithmStartButton = document.getElementById("algorithmStartButton");
@@ -18,7 +19,8 @@ algorithmStartButton.onclick = function() {
         getJsonData(cityGraphDataUri)
         .then(result => {
             algorithmCancelButton.style.visibility = "visible";
-            getResultsFromAlgorithm(0, result['uri']);
+            sessionStorage.setItem('uri', result['uri']);
+            getResultsFromAlgorithm(0, sessionStorage.getItem('uri'));
         })
         .catch(error => console.log(error));
     } else {
@@ -26,11 +28,22 @@ algorithmStartButton.onclick = function() {
     }
 }
 
+algorithmCancelButton.onclick = function() {
+    const uri = sessionStorage.getItem('uri');
+    deleteForUri(uri).then(result => {
+        // TODO handle here successfully cancelled task or in elseif in getResultsFromAlgorithm method
+        console.log(result);
+    })
+    .catch(error => console.log(error));
+}
+
 function getResultsFromAlgorithm(requestCounter, uri) {
     getJsonData(uri).then(result => {
         const calculationStatus = result['status'];
         if (calculationStatus == "SUCCESS") {
             getPositiveResultFromAlgorithm(result['taskId']);
+        } else if (calculationStatus == "CANCELED") {
+            console.log("Cancelled!");
         } else if (requestCounter < maxNumberOfRequestForCalculationStatus) {
             requestCounter++;
             setTimeout(getResultsFromAlgorithm, millisecondsToWaitBetweenRequests, requestCounter, uri);
